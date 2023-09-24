c11e3cb0426a68af6579c07a501bef8a90666235
awstone <alexstone977@gmail.com>
2023-09-23 23:37:29 -0400

    started adding macos configuration

--- flake.nix
+++ flake.nix
@@ -1,61 +1,70 @@
 {
-  description = "Your new nix config";
+    description = "Your new nix config";
 
-  inputs = {
-    # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    inputs = {
+	# Nixpkgs
+	nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
 
-    # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+	# MacOS stuff
+	nix-darwin.url = "github:LnL7/nix-darwin";
+        nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-    # TODO: Add any other flake you might need
-    # hardware.url = "github:nixos/nixos-hardware";
+	# Home manager
+	home-manager.url = "github:nix-community/home-manager/release-23.05";
+	home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    # Shameless plug: looking for a way to nixify your themes and make
-    # everything match nicely? Try nix-colors!
-    # nix-colors.url = "github:misterio77/nix-colors";
-  };
+	# TODO: Add any other flake you might need
+	# hardware.url = "github:nixos/nixos-hardware";
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-    ...
-  } @ inputs: let
+	# Shameless plug: looking for a way to nixify your themes and make
+	# everything match nicely? Try nix-colors!
+	# nix-colors.url = "github:misterio77/nix-colors";
+    };
+
+    outputs = {
+	self,
+	nixpkgs,
+	nix-darwin,
+	home-manager,
+	...
+    } @ inputs: let
     inherit (self) outputs;
-  in {
+	in {
 
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      "alex@rickybobby" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        # > Our main home-manager configuration file <
-        modules = [./home/alex/rickybobby.nix];
-      };
-      "alex@cappuccino" = {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-	extraSpecialArgs = {inherit inputs outputs;};
-	# > Our main home-manager configuration file <
-	modules = [./home/alex/cappuccino.nix];
-      };
-    };
+	    # Standalone home-manager configuration entrypoint
+	    # Available through 'home-manager --flake .#your-username@your-hostname'
+	    homeConfigurations = {
+		"alex@rickybobby" = home-manager.lib.homeManagerConfiguration {
+		    pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+		    extraSpecialArgs = {inherit inputs outputs;};
+		    modules = [./home/alex/rickybobby.nix];
+		};
+		"alex@cappuccino" = {
+		    pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+		    extraSpecialArgs = {inherit inputs outputs;};
+		    modules = [./home/alex/cappuccino.nix];
+		};
+	    };  
 
-    # NixOS configuration entrypoint
-    # Available through 'nixos-rebuild --flake .#your-hostname'
-    nixosConfigurations = {
-      rickybobby = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        # > Our main nixos configuration file <
-        modules = [./hosts/rickybobby/configuration.nix];
-      };
-      cappuccino = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-	# > Our main nixos configuration file <
-	modules = [./hosts/cappuccino/configuration.nix];
-      };  	
-    };
-  };
+	    # NixOS configuration entrypoint
+	    # Available through 'nixos-rebuild --flake .#your-hostname'
+	    nixosConfigurations = {
+		rickybobby = nixpkgs.lib.nixosSystem {
+		    specialArgs = {inherit inputs outputs;};
+		    modules = [./hosts/rickybobby/configuration.nix];
+		};
+		cappuccino = nixpkgs.lib.nixosSystem {
+		    specialArgs = {inherit inputs outputs;};
+		    modules = [./hosts/cappuccino/configuration.nix];
+		};
+	    };
+
+	    darwinConfigurations = {
+		beetlejuice = nix-darwin.lib.darwinSystem {
+		    inherit inputs;
+		    system = "aarch64-darwin";
+		    modules = [./hosts/beetlejuice/configuration.nix];
+		};
+	    };
+	};
 }
