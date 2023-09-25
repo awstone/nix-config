130f9b2539780753944f616fe13a031bad2c8305
awstone <alexstone977@gmail.com>
2023-09-25 16:55:03 -0400

    emacs config finally working

--- flake.nix
+++ flake.nix
@@ -1,83 +1,66 @@
 {
-    description = "Your new nix config";
+  description = "Your new nix config";
 
-    inputs = {
-	# Nixpkgs
-	nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+  inputs = {
+	  # Nixpkgs
+	  nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
 
-	# MacOS stuff
-	nix-darwin.url = "github:LnL7/nix-darwin";
-        nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+	  # MacOS stuff
+	  nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-	# Home manager
-	home-manager.url = "github:nix-community/home-manager/release-23.05";
-	home-manager.inputs.nixpkgs.follows = "nixpkgs";
+	  # Home manager
+	  home-manager.url = "github:nix-community/home-manager/release-23.05";
+	  home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-	# TODO: Add any other flake you might need
-	# hardware.url = "github:nixos/nixos-hardware";
+	  # TODO: Add any other flake you might need
+	  # hardware.url = "github:nixos/nixos-hardware";
 
-	# Shameless plug: looking for a way to nixify your themes and make
-	# everything match nicely? Try nix-colors!
-	# nix-colors.url = "github:misterio77/nix-colors";
-    };
+	  # Shameless plug: looking for a way to nixify your themes and make
+	  # everything match nicely? Try nix-colors!
+	  # nix-colors.url = "github:misterio77/nix-colors";
+  };
 
-    outputs = {
-	self,
-	nixpkgs,
-	nix-darwin,
-	home-manager,
-	...
-    } @ inputs: let
+  outputs = {
+	  self,
+	    nixpkgs,
+	    nix-darwin,
+	    home-manager,
+	    ...
+  } @ inputs: let
     inherit (self) outputs;
-	in {
+	in {  
 
-	    # Standalone home-manager configuration entrypoint
-	    # Available through 'home-manager --flake .#your-username@your-hostname'
-	    homeConfigurations = {
-		"alex@rickybobby" = home-manager.lib.homeManagerConfiguration {
-		    pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-		    extraSpecialArgs = {inherit inputs outputs;};
-		    modules = [
-		      ./home/alex/rickybobby.nix
-		    ];
-		};
-		"alex@cappuccino" = {
-		    pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-		    extraSpecialArgs = {inherit inputs outputs;};
-		    modules = [./home/alex/cappuccino.nix];
-		};
-	    };  
-
-	    # NixOS configuration entrypoint
-	    # Available through 'nixos-rebuild --flake .#your-hostname'
-	    nixosConfigurations = {
-		rickybobby = nixpkgs.lib.nixosSystem {
+	  # NixOS configuration entrypoint
+	  # Available through 'nixos-rebuild --flake .#your-hostname'
+	  nixosConfigurations = {
+		  rickybobby = nixpkgs.lib.nixosSystem {
 		    specialArgs = {inherit inputs outputs;};
 		    modules = [
 		      ./hosts/rickybobby/configuration.nix
 		      home-manager.nixosModules.home-manager
 		      {
-		        home-manager.useGlobalPkgs = true;
-			home-manager.useUserPackages = true;
-			home-manager.users.alex = import ./users/alex/home.nix;
+		        # home-manager.useGlobalPkgs = true;
+			      # home-manager.useUserPackages = true;
+			      home-manager.users.alex = import ./users/alex/home.nix;
 
-                        # Optionally, use home-manager.extraSpecialArgs to pass
-			# arguments to home.nix
-	              }
+            # Optionally, use home-manager.extraSpecialArgs to pass
+			      # arguments to home.nix
+	        }
 		    ];
-		};
-		cappuccino = nixpkgs.lib.nixosSystem {
+		  };
+		  cappuccino = nixpkgs.lib.nixosSystem {
 		    specialArgs = {inherit inputs outputs;};
 		    modules = [./hosts/cappuccino/configuration.nix];
-		};
-	    };
+		  };
+	  };
 
-	    darwinConfigurations = {
-		beetlejuice = nix-darwin.lib.darwinSystem {
+	  darwinConfigurations = {
+		  beetlejuice = nix-darwin.lib.darwinSystem {
 		    inherit inputs;
 		    system = "aarch64-darwin";
 		    modules = [./hosts/beetlejuice/configuration.nix];
-		};
-	    };
+		  };
+	  };
 	};
 }
--- users/alex/home.nix
+++ users/alex/home.nix
@@ -45,11 +45,107 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  # home.packages = with pkgs; [ slack ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
   programs.git.enable = true;
+  programs.emacs = {
+    enable = true;
+    package = pkgs.emacs29;
+    extraPackages = epkgs: [
+      epkgs.nix-mode
+      epkgs.magit
+      epkgs.company
+      epkgs.doom-themes
+      epkgs.lsp-mode
+      epkgs.org
+      epkgs.org-bullets
+    ];
+    extraConfig = ''
+      (require 'package)
+      (require 'company)
+      (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+      ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+      ;; and `package-pinned-packages`. Most users will not need or want to do this.
+      ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+      (package-initialize)
+      (custom-set-variables
+       ;; custom-set-variables was added by Custom.
+       ;; If you edit it by hand, you could mess it up, so be careful.
+       ;; Your init file should contain only one such instance.
+       ;; If there is more than one, they won't work right.
+       '(org-agenda-files '("~/Documents/1.org"))
+       '(package-selected-packages
+         '(org-bullets yafolding json-mode magit company lsp-mode taxy-magit-section ement doom-themes)))
+      (custom-set-faces
+       ;; custom-set-faces was added by Custom.
+       ;; If you edit it by hand, you could mess it up, so be careful.
+       ;; Your init file should contain only one such instance.
+       ;; If there is more than one, they won't work right.
+      )
+
+      (use-package doom-themes
+        :ensure t
+        :config
+        ;; Global settings (defaults)
+        (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+              doom-themes-enable-italic t) ; if nil, italics is universally disabled
+        (load-theme 'doom-xcode t)
+
+        ;; Enable flashing mode-line on errors
+        (doom-themes-visual-bell-config)
+        ;; Enable custom neotree theme (all-the-icons must be installed!)
+        (doom-themes-neotree-config)
+        ;; or for treemacs users
+        (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+        (doom-themes-treemacs-config)
+        ;; Corrects (and improves) org-mode's native fontification.
+        (doom-themes-org-config))
+
+      (setenv "PETSC_DIR" "~/Repositories/petsc")
+      (setenv "PETSC_ARCH" "arch-darwin-c-debug")
+      (global-company-mode t)
+      (setq global-display-line-numbers-mode t)
+      (setq company-idle-delay 0.5) ; set delay to 0.5 seconds
+      (require 'lsp-mode)
+      (add-hook 'python-mode-hook #'lsp)
+      (with-eval-after-load 'lsp-mode
+        (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\\\.pyenv\\\\.*")
+        (lsp-register-client
+         (make-lsp-client :new-connection (lsp-tramp-connection "/home/awstone/phonetic-flashcards/venv/bin/pyls")
+                          :major-modes '(python-mode)
+                          :remote? t
+                          :server-id 'pyls-remote)))
+      ;; -*- mode: elisp -*-
+
+      ;; Disable the splash screen (to enable it agin, replace the t with 0)
+      (setq inhibit-splash-screen t)
+
+      ;; Enable transient mark mode
+      (transient-mark-mode 1)
+
+      ;;;;Org mode configuration
+      ;; Enable Org mode
+      (require 'org)
+      ;; Make Org mode work with files ending in .org
+      ;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+      ;; The above is the default in recent emacsen
+      (setq org-todo-keywords
+        '((sequence "TODO" "IN-PROGRESS" "WAITING" "DONE")))
+      (global-set-key "\C-ca" 'org-agenda)
+      (setq org-tag-alist '(("@work" . ?w) ("@home" . ?h) ("laptop" . ?l)))
+      (org-babel-do-load-languages
+       'org-babel-load-languages
+       '((python . t)))
+      (setq org-babel-python-command "python3")
+      (setq org-confirm-babel-evaluate nil)
+      (use-package org-bullets
+        :config
+        (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+      (setq org-log-done t)
+    '';
+  };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
