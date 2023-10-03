3c9c65e0ae5f05d02d1d3ac285a6094d8b8f1e43
awstone <alexstone977@gmail.com>
2023-10-03 00:10:14 -0400

    removed dns for ssh because it was making emacs really slow

--- hosts/rickybobby/configuration.nix
+++ hosts/rickybobby/configuration.nix
@@ -120,6 +120,7 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+  services.openssh.extraConfig = '' UseDNS no '';
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
