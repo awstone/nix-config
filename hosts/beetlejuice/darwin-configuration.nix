9a6e94eb1281e1215ebf4888e3a32ef4abad0ca2
awstone <alexstone977@gmail.com>
2023-10-22 12:31:16 -0400

    I think nix-darwin is working now

--- hosts/beetlejuice/darwin-configuration.nix
+++ hosts/beetlejuice/darwin-configuration.nix
@@ -20,7 +20,8 @@
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
+  nix.package = pkgs.nixUnstable;
+  # services.nix-daemon.package = pkgs.nixFlakes;
 
   # Create /etc/zshrc that loads the nix-darwin environment.
   programs.zsh.enable = true;  # default shell on catalina
